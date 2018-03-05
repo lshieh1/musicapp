a0226fc1140bfc466629ce60337c11da75ce37a2
Lillian Shieh <lillian.shieh95@gmail.com>
2018-03-05 09:13:30 -0500

    heroku commit

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,7 @@
 module.exports = process.env.DATABASE_URL || {
-  host:     process.env.DB_HOST || 'localhost',
-  port:     process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'musicapp_db',
+  host:     process.env.PG_HOST || 'localhost',
+  port:     process.env.PG_PORT || 5432,
+  database: process.env.PG_DATABASE || 'musicapp_db',
   username: 'lillian',
   password: '123'
 };
